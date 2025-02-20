9c8cf1c48e439970d4c177257ac4c8b89cee4aa6
MariaEdSilva <161876391+MariaEdSilva@users.noreply.github.com>
2025-02-20 16:39:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Aguarda até que o documento HTML seja totalmente carregado antes de executar o script
+document.addEventListener("DOMContentLoaded", function(){
+    //Obtém referências aos elementos do formulário e lista
+    const form=document.getElementById("registrationForm");
+    const nameInput=document.getElementById("name");
+    const emailInput=document.getElementById("email");
+    const messegeDiv=document.getElementById("message");
+    const userList=document.getElementById("userList");
+
+    //Adiciona um evento de envio ao formulario
+    form.addEventListener("submit",)
+})
